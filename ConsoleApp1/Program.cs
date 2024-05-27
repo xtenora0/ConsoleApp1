5c5d0d644260e96199babf0dee057d6e94b19ddc
Karel Tenora <karel.tenora@daikindevice.cz>
2024-05-27 10:32:00 +0200

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string name = "";
+
+Console.WriteLine("Jak se jmenujes?");
+name = Console.ReadLine();
+
+switch (name) 
+{
+    case "Lukas":
+        Console.WriteLine("Ahoj Lukasi");
+        break;
+    case "Karel":
+        Console.WriteLine("Ahoj Karle");
+        break;
+    case "Tomas":
+        Console.WriteLine("Ahoj Tomasi");
+        break;
+    case "Martin":
+        Console.WriteLine("Ahoj Martine");
+        break;
+    case "Radim":
+        Console.WriteLine("Ahoj Radime");
+        break;
+    case "Jakub":
+        Console.WriteLine("Ahoj Jakube");
+        break;
+    case "Sofian":
+        Console.WriteLine("Ahoj Sofiane");
+        break;
+    default:
+        Console.WriteLine("Neznam te");
+        break;
+
+}
