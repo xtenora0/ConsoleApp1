baa23f687be7dc6b19e1a76071f2e73fb8cddcc7
Karel Tenora <karel.tenora@daikindevice.cz>
2024-05-27 10:55:14 +0200

    animal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,35 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string name = "";
+//string name = "";
 
-Console.WriteLine("Jak se jmenujes?");
-name = Console.ReadLine();
+//Console.WriteLine("Jak se jmenujes?");
+//name = Console.ReadLine();
 
-switch (name) 
-{
-    case "Lukas":
-        Console.WriteLine("Ahoj Lukasi");
-        break;
-    case "Karel":
-        Console.WriteLine("Ahoj Karle");
-        break;
-    case "Tomas":
-        Console.WriteLine("Ahoj Tomasi");
-        break;
-    case "Martin":
-        Console.WriteLine("Ahoj Martine");
-        break;
-    case "Radim":
-        Console.WriteLine("Ahoj Radime");
-        break;
-    case "Jakub":
-        Console.WriteLine("Ahoj Jakube");
-        break;
-    case "Sofian":
-        Console.WriteLine("Ahoj Sofiane");
-        break;
-    default:
-        Console.WriteLine("Neznam te");
-        break;
+//switch (name) 
+//{
+//    case "Lukas":
+//        Console.WriteLine("Ahoj Lukasi");
+//        break;
+//    case "Karel":
+//        Console.WriteLine("Ahoj Karle");
+//        break;
+//    case "Tomas":
+//        Console.WriteLine("Ahoj Tomasi");
+//        break;
+//    case "Martin":
+//        Console.WriteLine("Ahoj Martine");
+//        break;
+//    case "Radim":
+//        Console.WriteLine("Ahoj Radime");
+//        break;
+//    case "Jakub":
+//        Console.WriteLine("Ahoj Jakube");
+//        break;
+//    case "Sofian":
+//        Console.WriteLine("Ahoj Sofiane");
+//        break;
+//    default:
+//        Console.WriteLine("Neznam te");
+//        break;
 
-}
+//}
+
+using ConsoleApp1;
+
+Zvire zvire1 = new Zvire("Selma", "Kocka");
+zvire1.RekniCoJsi();
+Zvire zvire2 = new Zvire("Psovity", "Pes");
+zvire2.RekniCoJsi();
---
+++ ConsoleApp1/Zvire.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+
+public class Zvire
+{
+    public string druh;
+    public string jmeno;
+
+    public Zvire(string _druh, string _jmeno) 
+    {
+        this.druh = _druh;
+        this.jmeno = _jmeno; 
+    }
+
+    public void RekniCoJsi() 
+    {
+        Console.WriteLine("Jsem " + druh + " a " + jmeno);
+    }
+}
