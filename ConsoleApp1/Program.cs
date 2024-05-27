5d09a5ec27d5ffd857fe56b619a60f500ac9f9ad
Karel Tenora <karel.tenora@daikindevice.cz>
2024-05-27 13:34:54 +0200

    verze1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] jmena = ["Lukáš", "Karel", "Tomáš", "Martin", "Radim", "Jakub", "Sofián", "Pavel", "Petr"];
+//string[] jmena = ["Lukáš", "Karel", "Tomáš", "Martin", "Radim", "Jakub", "Sofián", "Pavel", "Petr"];
 
-List<string> listJmena = new List<string>() {
-    "Lukáš", "Karel", "Tomáš", "Martin", "Radim", "Jakub", "Sofián", "Pavel", "Petr"
+//List<string> listJmena = new List<string>() {
+//    "Lukáš", "Karel", "Tomáš", "Martin", "Radim", "Jakub", "Sofián", "Pavel", "Petr"
+//};
+
+//List<string> listJmena2 = ["Lukáš", "Karel", "Tomáš", "Martin", "Radim", 
+//    "Jakub", "Sofián", "Pavel", "Petr"];
+
+//List<string> listJmena3 = new(){"Lukáš", "Karel", "Tomáš", "Martin", "Radim",
+//    "Jakub", "Sofián", "Pavel", "Petr"};
+
+//listJmena.Add("Ester");
+
+//Console.WriteLine("Pocet lidi v seznamu: " + listJmena.Count);
+//foreach (var item in listJmena) 
+//{
+//    Console.WriteLine(item + " pocet pismen: " + item.Length);
+//}
+
+//Dictionary<int, string> dnyVTydnu = new Dictionary<int, string>() 
+//{
+//    {1, "Pondeli" },
+//    {2, "Utery" },
+//    {3, "Streda" },
+//    {4, "Ctvrtek" },
+//    {5, "Patek" },
+//    {6, "Sobota" },
+//    {7, "Nedele" },
+//};
+
+//var streda = dnyVTydnu[3];
+
+Dictionary<string, string> jmenaPozdravy = new Dictionary<string, string>()
+{
+    {"Lukas", "Lukasi" },
+    {"Karel", "Karle" },
+    {"Tomas", "Tomasi" },
+    {"Martin", "Martine" },
+    {"Radim", "Radime" },
+    {"Jakub", "Jakube" },
+    {"Sofian", "Sofiane" },
 };
 
-List<string> listJmena2 = ["Lukáš", "Karel", "Tomáš", "Martin", "Radim", 
-    "Jakub", "Sofián", "Pavel", "Petr"];
+Console.WriteLine("Jak se jmenujes?");
+string jmeno = Console.ReadLine();
 
-List<string> listJmena3 = new(){"Lukáš", "Karel", "Tomáš", "Martin", "Radim",
-    "Jakub", "Sofián", "Pavel", "Petr"};
-
-listJmena.Add("Ester");
-
-Console.WriteLine("Pocet lidi v seznamu: " + listJmena.Count);
-foreach (var item in listJmena) 
+switch (jmeno)
 {
-    Console.WriteLine(item + " pocet pismen: " + item.Length);
+    case "Lukas":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Karel":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Tomas":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Martin":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Radim":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Jakub":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    case "Sofian":
+        Console.WriteLine("Ahoj " + jmenaPozdravy[jmeno]);
+        break;
+    default:
+        Console.WriteLine("Neznam te");
+        break;
+
 }
+
 
 //for(int i = 0; i < jmena.Length; i++) 
 //{
