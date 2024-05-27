e8f05808623c376b1b8fefcff7d9cb06b88f5fa5
Karel Tenora <karel.tenora@daikindevice.cz>
2024-05-27 15:03:54 +0200

    update Any/All, anonymni typy

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -178,7 +178,7 @@ using System.Linq;
 //-----------------------------------------------------------------------------------
 //Linq
 
-int[] numbers = [12, 44, 43, -69, 66, 32, 3, 0, -8, 6, -8, 80,53,-63,36,5,0,75,-75];
+int[] numbers = [12, 44, 43, -69, 66, 32, 3, 0, -8, 6, -8, 80,53,-63,36,5,0,75,-75, -120];
 
 //WHERE filtr
 var kladna777 = numbers.Where(numbers => numbers > 0).ToList();
@@ -201,23 +201,45 @@ var max = numbers.Max(numbers => numbers);
 var positiveNum = numbers.Select(number => Math.Abs(number)).Where(number => number > 50);
 //Console.WriteLine(positiveNum);
 
-//ukoly
-var pocetKladna = numbers.Where(numbers => numbers > 0).Count();
-var pocetZaporna = numbers.Where(numbers => numbers < 0).Count();
-var sumKlad = numbers.Where(numbers => numbers > 0).Sum();
-var maxAbs = numbers.Select(number => Math.Abs(number)).Max();
-var kladSud = numbers.Where(numbers => numbers > 0 && numbers % 2 == 0).ToList();
-var orderedByDesc = numbers.OrderDescending();
-var skipThreeSum = numbers.Skip(3).Sum();
+////ukoly
+//var pocetKladna = numbers.Where(numbers => numbers > 0).Count();
+//var pocetZaporna = numbers.Where(numbers => numbers < 0).Count();
+//var sumKlad = numbers.Where(numbers => numbers > 0).Sum();
+//var maxAbs = numbers.Select(number => Math.Abs(number)).Max();
+//var kladSud = numbers.Where(numbers => numbers > 0 && numbers % 2 == 0).ToList();
+//var orderedBy = numbers.OrderBy(num => num);
+//var skipThreeSum = numbers.Skip(3).Sum();
 
-Console.WriteLine($"Kladna: {pocetKladna}");
-Console.WriteLine($"Zaporna: {pocetZaporna}");
-Console.WriteLine($"Suma kladnych: {sumKlad}");
-Console.WriteLine($"Max absolutni : {maxAbs}");
-Console.WriteLine($"Kladna suda cisla: {string.Join(", ",kladSud)}");
-Console.WriteLine($"Serazeno od nejmensi po nejvetsi: {string.Join(", ", orderedByDesc)}");
-Console.WriteLine($"Preskoc 3 a secti: {skipThreeSum}");
+//Console.WriteLine($"Kladna: {pocetKladna}");
+//Console.WriteLine($"Zaporna: {pocetZaporna}");
+//Console.WriteLine($"Suma kladnych: {sumKlad}");
+//Console.WriteLine($"Max absolutni : {maxAbs}");
+//Console.WriteLine($"Kladna suda cisla: {string.Join(", ",kladSud)}");
+//Console.WriteLine($"Serazeno od nejmensi po nejvetsi: {string.Join(", ", orderedBy)}");
+//Console.WriteLine($"Preskoc 3 a secti: {skipThreeSum}");
 
+////All, Any
+
+//bool vsechna_kladna = numbers.All(num => num > 0);
+//bool alespon_jedno_kladne = numbers.Any(num => num > 0);
+
+//pismena dohromady
+var fruits = new[] { "aPPle", "BluebERRY", "chERRy", "RaspBERRy" };
+var dohromady_pismen = fruits.Select(f => f.Length).Sum();
+Console.WriteLine($"Dohromady pismen {dohromady_pismen}");
+
+var upperFruits = fruits.Select(x => x.ToUpper());
+Console.WriteLine($"Fruits upper: {string.Join(", ", upperFruits)}");
+var lowerFruits = fruits.Select(x => x.ToLower());
+Console.WriteLine($"Fruits lower: {string.Join(", ", lowerFruits)}");
+
+//Upper lower case combo
+var fruit_combo = fruits.Select(x => new
+{
+    Uppercase = x.ToUpper(),
+    Lowercase = x.ToLower(),
+});
+Console.WriteLine($"Fruits upper lower combo: {string.Join(", ", fruit_combo)}");
 
 //foreach (var n in kladna)
 //{
